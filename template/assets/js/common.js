f48432f0453a0a697c5c5503ff2c6a065166f7a1
EnthusiasticCitizen <peng@sharecreators.com>
2022-05-11 10:46:37 +0800

    兼容移动端

--- template/assets/js/common.js
+++ template/assets/js/common.js
@@ -1,32 +1,39 @@
-;(function () {
-    function check(data = []) {
-        let flag = true
-        data.every((item) => {
-            let isCheck = item.getAttribute('data-require')
-            if (isCheck && !item.value) {
-                flag = false
-                return false
+//定义一个函数判断是手机端还是pc端
+function isMobile() {
+    if (
+        window.navigator.userAgent.match(
+            /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+        )
+    ) {
+        return true // 移动端
+    } else {
+        return false // PC端
+    }
+}
+function check(data = []) {
+    let flag = true
+    data.every((item) => {
+        let isCheck = item.getAttribute('data-require')
+        if (isCheck && !item.value) {
+            flag = false
+            return false
+        }
+        return true
+    })
+    return flag
+}
+const sendEmail = (e) => {
+    e.preventDefault()
+    let flag = check && check(Array.from(e.target))
+    flag &&
+        emailjs.sendForm(apiKeys.SERVICE_ID, apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID).then(
+            (result) => {
+                console.log(result.text)
+                alert('发送成功！')
+            },
+            (error) => {
+                console.log(error.text)
+                alert('发送失败！')
             }
-            return true
-        })
-        return flag
-    }
-    const sendEmail = (e) => {
-        e.preventDefault()
-        let flag = check && check(Array.from(e.target))
-        flag &&
-            emailjs.sendForm(apiKeys.SERVICE_ID, apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID).then(
-                (result) => {
-                    console.log(result.text)
-                    alert('发送成功！')
-                },
-                (error) => {
-                    console.log(error.text)
-                    alert('发送失败！')
-                }
-            )
-    }
-
-    const form = document.querySelector('#form')
-    form.addEventListener('submit', sendEmail)
-})()
+        )
+}
